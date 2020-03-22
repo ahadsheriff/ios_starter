6f178be5f70f773858ca27594f315cfd909de88b
Ahad Sheriff <ahadsheriff@gmail.com>
2020-03-22 16:16:59 -0700

    initial commit

---
+++ starter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  starter
+//
+//  Created by Ahad Sheriff on 3/22/20.
+//  Copyright © 2020 Ahad Sheriff. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
